35eaba1e1e99058c7387d839b8acf971c41719dc
Bradley Morton <bradley.m.morton@gmail.com>
2018-09-24 18:06:52 -0800

    Initial programs with some comments changed.

---
+++ Project_03/da3.cpp
@@ -0,0 +1,19 @@
+// da3.cpp
+// Bradley Morton
+//Skeleton provided by Glenn G. Chappell
+// 24 Sep 2018
+//
+// For CS 311 Fall 2018
+// Source for Project 3 Functions
+
+#include "da3.h"       // For Project 3 prototypes & templates
+
+
+int modExp(int a,
+           int b,
+           int n)
+{
+    return 42;  // Dummy return
+    // TODO: Write this!!!
+}
+
