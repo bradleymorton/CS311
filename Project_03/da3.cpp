3f79b06e6f67a8c6677959c1a99a775695d9d603
Bradley <bmmorton@alaska.edu>
2018-09-27 14:00:33 -0800

    Rewrote modExp, it no longer calls another function.

--- Project_03/da3.cpp
+++ Project_03/da3.cpp
@@ -9,19 +9,6 @@
 #include "da3.h"       // For Project 3 prototypes & templates
 
 
-//This function is recursive and called by the 
-//modExp function, and cannot be called by the 
-//test program, or any other program linked to these
-//files. 
-int exp(std::size_t base, std::size_t exponent)
-{
-	if(exponent==1)
-	{
-		return base;
-	}
-	else return base*exp(base, exponent-1);
-}
-
 
 //This function is prototyped in da3.h, and is to be called by
 //the test program.
@@ -29,24 +16,26 @@ int modExp(int a,
            int b,
            int n)
 {
-	std::size_t aa=a;
-	std::size_t bb=b;
-	std::size_t nn=n;
-    if(n==1)
-    {
-    	return 0;
-    }
-    else if(b==0)
+	a=a%n;
+    if(b==0&&n%2==0)
     {
     	return 1;
     }
+    else if(b==0&&n%2==1)
+    {
+    	return 0;
+    }
+    else if(b==1)
+    {
+    	return a;
+    }
     else if(b%2==0)
     {
-    	return exp(aa,bb/2)*exp(aa,bb/2)%nn;
+    	return modExp(a*a%n,b/2,n);
     }
     else
     {
-    	return ((aa%nn)*(exp(aa,bb/2)*exp(aa,bb/2))%nn)%nn;
+    	return a*modExp(a,b-1,n)%n;
     }
 }
 
