e84c522ad8302f6148e40af233ca55d6ce58bfbf
Bradley Morton <bradley.m.morton@gmail.com>
2018-09-30 20:18:04 -0800

    Added initial comments.

---
+++ Project_4/counthdt.cpp
@@ -0,0 +1,10 @@
+//counthdt.cpp
+//Bradley Morton
+//9/30/18
+//Contains definitions of functions for Assignment 4
+
+
+#include "counthdt.h"
+
+
+
---
+++ Project_4/counthdt.h
@@ -0,0 +1,17 @@
+//counthdt.h
+//Bradley Morton
+//9/30/18
+//Contains prototypes of functions for Assignment 4
+
+
+#ifndef COUNTHDT_H
+#define COUNTHDT_H
+
+
+
+
+
+
+
+
+#endif
