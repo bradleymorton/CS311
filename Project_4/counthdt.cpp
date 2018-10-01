2347a1c011676a4c30ccb41e79706cbe7eb10b71
Bradley Morton <bradley.m.morton@gmail.com>
2018-09-30 22:20:04 -0800

    Writing functions.

--- Project_4/counthdt.cpp
+++ Project_4/counthdt.cpp
@@ -4,7 +4,34 @@
 //Contains definitions of functions for Assignment 4
 
 
-#include "counthdt.h"
+#include "counthdt.h" //For prototypes
+#include <vector>
+using std::vector;
 
 
+int countHDT(int dim_x, int dim_y, int forbid1_x, int forbid1_y, int forbid2_x, int forbid2_y)
+{
+
+	vector<vector<int>> board(dim_x, std::vector<int>(dim_y,0));
+	board[forbid1_x][forbid1_y]=1;
+	board[forbid2_x][forbid2_y]=1;
+	return 0;
+}
+
+int countHDT_recurse(vector<vector<int>> board, int dim_x, int dim_y, int squaresLeft)
+{
+
+	if(squaresLeft==0)
+	{
+		return 1;
+	}
+	else
+	{
+
+		//Stuff
+		return countHDT_recurse(board, dim_x, dim_y, squaresLeft-2);
+	}
+	return 0;
+
+}
 
--- Project_4/counthdt.h
+++ Project_4/counthdt.h
@@ -7,7 +7,13 @@
 #ifndef COUNTHDT_H
 #define COUNTHDT_H
 
+#include <vector>
+using std::vector;
 
+int countHDT(int dim_x, int dim_y, int forbid1_x, int forbid1_y, int forbid2_x, int forbid2_y);
+
+
+int countHDT_recurse(vector<vector<int>> board, int dim_x, int dim_y, int squaresLeft);
 
 
 
