8486e9936a90217dfe7b3279c9d2e968ceb67ad9
Bradley Morton <bradley.m.morton@gmail.com>
2018-09-30 22:53:05 -0800

    Making some progress. Code currently crashes.

--- Project_4/counthdt.cpp
+++ Project_4/counthdt.cpp
@@ -15,7 +15,9 @@ int countHDT(int dim_x, int dim_y, int forbid1_x, int forbid1_y, int forbid2_x, 
 	vector<vector<int>> board(dim_x, std::vector<int>(dim_y,0));
 	board[forbid1_x][forbid1_y]=1;
 	board[forbid2_x][forbid2_y]=1;
-	return 0;
+	countHDT_recurse(board, dim_x, dim_y, dim_y*dim_x);
+	int total=0;
+	return total;
 }
 
 int countHDT_recurse(vector<vector<int>> board, int dim_x, int dim_y, int squaresLeft)
@@ -25,12 +27,36 @@ int countHDT_recurse(vector<vector<int>> board, int dim_x, int dim_y, int square
 	{
 		return 1;
 	}
-	else
+
+	int total=0;
+	int i=0, j=0;
+	for(i; i<dim_x; ++i)
 	{
+		for(j; j<dim_y; ++j)
+		{
+			if(board[i][j]==1)
+					continue;
+			if(i+1<dim_x&&board[i-1][j]==0)
+			{
+				board[i][j]=1;
+				board[i+1][j]=1;
+				return countHDT_recurse(board, dim_x, dim_y, squaresLeft-2);
+				board[i][j]=0;
+				board[i+1][j]=0;
+			}	
+			if(j+1<dim_y&&board[i][j-1]==0)
+			{
+				board[i][j]=1;
+				board[i][j+1]=1
+				return countHDT_recurse(board, dim_x, dim_y, squaresLeft-2);
+				board[i][j]=0;
+				board[i][j+1]=0;
+			}			
+		}
+	}
 
 		//Stuff
-		return countHDT_recurse(board, dim_x, dim_y, squaresLeft-2);
-	}
+		return total
 	return 0;
 
 }
