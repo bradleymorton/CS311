f1f059ecc9fb2c2baa29038a7d0fd17cc7362ace
Ian Ferguson <imferguson@alaska.edu>
2018-10-30 12:32:52 -0800

    Added a standard definition for the header file.

--- Project_5/tvsarray.h
+++ Project_5/tvsarray.h
@@ -8,6 +8,141 @@
 #ifndef TVSARRAY_H_INCLUDED
 #define TVSARRAY_H_INCLUDED
 
-
+#include <cstddef>
+// For std::size_t
+#include <algorithm>
+// For std::max;
+// *********************************************************************
+// class VSArray - Class definition
+// *********************************************************************
+// class VSArray
+// Very Smart Array of int.
+// Resizable, copyable/movable, exception-safe.
+// Invariants:
+//     0 <= _size <= _capacity.
+//     _data points to an array of int, allocated with new[], owned by
+//      *this, holding _capacity ints. Except: _data may be nullptr, if
+//      _capacity == 0.
+class VSArray {
+    // ***** VSArray: types *****
+public:
+    // value_type: type of data items
+    using value_type = int;
+    // size_type: type of sizes & indices
+    using size_type = std::size_t;
+    // iterator, const_iterator: random-access iterator types
+    using iterator = value_type *;
+    using const_iterator = const value_type *;
+    // ***** VSArray: internal-use constants *****
+private:
+    // Capacity of default-constructed object
+    enum { DEFAULT_CAP = 16 };
+    // ***** VSArray: ctors, dctor, op= *****
+public:
+    // Default ctor & ctor from size
+    // Strong Guarantee
+    explicit VSArray(size_type size=0)
+    :_capacity(std::max(size, size_type(DEFAULT_CAP))),
+    // _capacity must be declared before _data
+    _size(size),
+    _data(new value_type[_capacity])
+    {}
+    // Copy ctor
+    // Strong Guarantee
+    VSArray(const VSArray & other);
+    // Move ctor
+    // No-Throw Guarantee
+    VSArray(VSArray && other) noexcept;
+    // Copy assignment operator
+    // ??? Guarantee
+    VSArray & operator=(const VSArray & other);
+    // Move assignment operator
+    // No-Throw Guarantee
+    VSArray & operator=(VSArray && other) noexcept;
+    // Dctor
+    // No-Throw Guarantee
+    ~VSArray()
+    {
+        delete [] _data;
+    }
+    // ***** VSArray: general public operators *****
+public:
+    // operator[] - non-const & const
+    // No-Throw Guarantee
+    value_type & operator[](size_type index)
+    {
+        return _data[index];
+    }
+    const value_type & operator[](size_type index) const
+    {
+        return _data[index];
+    }
+    // ***** VSArray: general public functions *****
+public:
+    // size
+    // No-Throw Guarantee
+    size_type size() const
+    {
+        return _size;
+    }
+    // empty
+    // No-Throw Guarantee
+    bool empty() const
+    {
+        return size() == 0;
+    }
+    // begin - non-const & const
+    // No-Throw Guarantee
+    iterator begin()
+    {
+        return _data;
+    }
+    const_iterator begin() const
+    {
+        return _data;
+    }
+    // end - non-const & const
+    // No-Throw Guarantee
+    iterator end()
+    {
+        return begin() + size();
+    }
+    const_iterator end() const
+    {
+        return begin() + size();
+    }
+    // resize
+    // ??? Guarantee
+    void resize(size_type newsize);
+    // insert
+    // ??? Guarantee
+    iterator insert(iterator pos,
+                    const value_type & item);
+    // erase
+    // ??? Guarantee
+    iterator erase(iterator pos);
+    // push_back
+    // InsertEnd operation.
+    // ??? Guarantee
+    void push_back(const value_type & item)
+    {
+        insert(end(), item);
+    }
+    // pop_back
+    // RemoveEnd operation.
+    // ??? Guarantee
+    void pop_back()
+    {
+        erase(end()-1);
+    }
+    // swap
+    // No-Throw Guarantee
+    void swap(VSArray & other) noexcept;
+    // ***** VSArray: data members *****
+private:
+    size_type    _capacity;  // Size of our allocated array
+    size_type    _size;      // Size of client's data
+    value_type * _data;      // Pointer to array
+};  // End class VSArray
 
 #endif // TVSARRAY_H_INCLUDED
