5dc88d2613b39e3637064459840cd5879cd9d455
Ian Ferguson <imferguson@alaska.edu>
2018-11-06 12:41:54 -0900

    Added the class and what have ya.

--- Project_6/da6.h
+++ Project_6/da6.h
@@ -1,8 +1,40 @@
 //Project 6 for CS311
 //Andrew Adler, Ian Ferguson, Bradley Morton
 
+#ifndef DA6_H_INCLUDED
+#define DA6_H_INCLUDED
+
 #include "llnode2.h"
+#include <algorithm>
 //For lldone.h
 
 template<typename ValType>
 void reverseList(shared_ptr<LLNode2<ValType>> & head);
+
+
+template <typename Key, typename Data>
+class ListMap {
+    
+public:
+    
+    using key_type= Key;
+    using data_type= Data;
+    
+public:
+    ListMap();
+    ~ListMap();
+    ListMap(const ListMap & base) = delete;
+    ListMap(ListMap && other) = delete;
+    ListMap & operator=(const ListMap & base) = delete;
+    ListMap & operator=(const ListMap && other) = delete;
+    int size();
+    ListMap find();
+    void insert(key_type key, data_type item);
+    void erase(key_type key);
+    ListMap traverse(std::function<void(key_type, data_type)> function);
+    
+private:
+    std::shared_ptr<LLNode2<std::pair<key_type, data_type>>> _head;
+};
+
+#endif //DA6_H_INCLUDED
