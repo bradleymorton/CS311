0a713764c0b638cda88a0590d06cba3d62fa76e3
bradley <bradley.m.morton@gmail.com>
2018-11-06 12:49:42 -0900

    made a couple changes.

--- Project_6/da6.h
+++ Project_6/da6.h
@@ -24,18 +24,23 @@ public:
     
 public:
     ListMap();
-    ~ListMap();
+    ~ListMap()
+    {}
     ListMap(const ListMap & base) = delete;
     ListMap(ListMap && other) = delete;
     ListMap & operator=(const ListMap & base) = delete;
     ListMap & operator=(const ListMap && other) = delete;
-    int size();
+    int size()
+    {
+        return _size;
+    }
     ListMap find();
     void insert(key_type key, data_type item);
     void erase(key_type key);
     ListMap traverse(std::function<void(key_type, data_type)> function);
     
 private:
+    int _size;
     std::shared_ptr<LLNode2<std::pair<key_type, data_type>>> _head;
 };
 
