4c04d6e7f4ebf05038ab73ec5566a3c60f82dde4
Ian Ferguson <imferguson@alaska.edu>
2018-12-03 16:43:12 -0900

    I can't run this because Xcode is a terrible ide.

--- Project_8/followers.cpp
+++ Project_8/followers.cpp
@@ -6,6 +6,46 @@
 //  Copyright © 2018 Ian Ferguson. All rights reserved.
 //
 
-#include <stdio.h>
 #include <iostream>
 #include <map>
+#include <fstream>
+#include <iterator>
+
+using std::fstream;
+using std::string;
+using std::map;
+using std::iterator;
+
+int main()
+{
+    std::cout << "Dick." << std::endl;
+    fstream fs ("followers_text.txt");
+    
+    map<string, int> mp;
+    
+    string words;
+    while(fs >> words)
+    {
+        if(!mp.count(words))
+        {
+            mp.insert(make_pair(words, 1));
+        }
+        else
+        {
+            mp[words]++;
+        }
+    }
+    fs.close();
+    
+    for(map<string, int> :: iterator p = mp.begin(); p != mp.end(); p++)
+        {
+        if (p -> second ==1)
+        {
+            std::cout << p-> first << std::endl;
+        }
+        }
+    return 0;
+
+}
+
+
